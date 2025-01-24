3a27cc24ba4c082c4d0d6c4a3e5d388ac2ff3855
GDMontsho <montshogoitseone@gmail.com>
2025-01-24 10:17:22 +0100

    update state for dashboard

--- src/WeatherDashboard.tsx
+++ src/WeatherDashboard.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { fetchWeatherData } from "./weatherApi";
 
-import React from "react";
-
 const WeatherDashboard = () => {
+  //state for bringing data into our dashboard when user needs it
+  const [city, setCity] = useState<String>("");
+  const [weather, setWeather] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [erro, setError] = useState<string | null>(null);
+
   return <div>WeatherDashboard</div>;
 };
 
